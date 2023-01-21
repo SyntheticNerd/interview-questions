97623679fda59932ea78368c6a79b23511cf0a50
SyntheticDesigner <andrew.sch.j@gmail.com>
2023-01-21 15:46:18 -0800

    ajs-004-complete adding elemtns to arrays and concatenate array

---
+++ adding elements to array/app.js
@@ -0,0 +1,3 @@
+// Write a function which get's an array and an element and returns a array with this element at the end.
+const append = (array, element) => [...array, element];
+// push modifies the original array which we dont want to happen
---
+++ concatenate 2 arrays/app.js
@@ -0,0 +1,2 @@
+// Write a function which can concatenate 2 arrays
+const concatenate = (arr1, arr2) => [...arr1, ...arr2];
